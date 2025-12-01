9ca7167984733494135e2a2bf92849ad4e0b8e2d
Wouter Mooij <wmooij@gmail.com>
2025-12-02 00:27:38 +0100

    Day 1 - Part 2 maar nog niet af

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -2,23 +2,36 @@ startposition = 50
 pos = startposition
 password = 0   
 
-f = open('input.txt', 'r')
+f = open('input copy.txt', 'r')
 lines = f.readlines()
 
 def rotate_position(pos, direction, steps):
+    nr_of_rounds = 0
     if len(str(steps)) > 2:
         # Beperk stappen tot de laatste 2 cijfers
+        print('org steps ' + str(steps))
+        print('org nr of rounds ' + str(nr_of_rounds))
+
+        nr_of_rounds += int(str(steps)[::-1][2:][::-1])
+        print('new nr of rounds ' + str(nr_of_rounds))
         steps = int(str(steps)[::-1][:2][::-1])
- 
+        print('new steps ' + str(steps))
     if direction == 'L':
         pos -= steps
         if pos < 0:
                pos = 100 - abs(pos)  
+               nr_of_rounds += 1
     elif direction == 'R':
         pos += steps
         if pos >= 100:
                pos= int(str(pos)[1:])
-    return pos 
+               nr_of_rounds += 1
+    print('Steps: ' + str(steps))
+    print('Direction: ' + direction)
+    print('Pos: ' + str(pos))
+    print('Nr of rounds: ' + str(nr_of_rounds))
+    pos = pos
+    return pos
 
 
 for line in lines:
